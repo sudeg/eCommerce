08113622a12df71faadd33ea133177dd4428088f
sudeg <sudegonul95@gmail.com>
2020-05-03 16:28:57 +0300

    cart totals completed u nasty

--- cart/models.py
+++ cart/models.py
@@ -104,6 +104,26 @@ class Order(models.Model):
     def reference_number(self):
         return f"ORDER-{self.pk}"
 
+    def get_raw_subtotal(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_raw_total_item_price()
+        return total
+
+    def get_subtotal(self):
+        subtotal = self.get_raw_subtotal()
+        return "{:.2f}".format(subtotal / 100)
+
+    def get_raw_total(self):
+        subtotal = self.get_raw_subtotal()
+        #total = subtotal - discounts + tax + delivery
+        # bu tax vs vs ekledikten sonra return total yapıcaksın
+        return subtotal
+
+    def get_total(self):
+        total = self.get_raw_total()
+        return "{:.2f}".format(total / 100)
+
 
 class Payment(models.Model):
     order = models.ForeignKey(
