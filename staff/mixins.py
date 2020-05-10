eaaa691c2e67341eda2ce7baeee26a0c0109739b
sudeg <sudegonul95@gmail.com>
2020-05-10 23:29:32 +0300

    html changes

--- staff/mixins.py
+++ staff/mixins.py
@@ -4,5 +4,5 @@ from django.shortcuts import redirect
 class StaffUserMixin(object):
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_staff:
-            return redirect("home")
+            return redirect("index")
         return super(StaffUserMixin, self).dispatch(request, *args, **kwargs)
