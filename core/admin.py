1e64a94291d9c924d4ab660e180bc95c3b056b39
sudeg <sudegonul95@gmail.com>
2020-06-01 23:41:10 +0300

    fd

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import *
+from django.template import loader
+from django.utils.html import format_html
+from django.shortcuts import render, redirect
+from django.urls import reverse, path
+from django.http import HttpResponse
+from django import forms
+import sys
+import json
+import requests
+import urllib
+import csv
+import io
 
 
 admin.site.register(Designer)
